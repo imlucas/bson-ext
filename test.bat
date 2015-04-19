8472b74d618c42584f8e653775fda647ce089f8e
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-04-19 12:32:43 -0400

    debugging

--- test.bat
+++ test.bat
@@ -1,9 +1,10 @@
 call node --version
 call npm --version
-set BASE=%cd%
-SET PATH=%PATH%;%BASE%\node_modules\.bin;%APPDATA%\npm
+SET GYP_MSVS_VERSION=2013
+SET PATH=%PATH%;%APPDATA%\npm
 SET PATH=%PATH%;C:\Program Files (x86)\Git\bin
+SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin
 call npm install https://github.com/mongodb-js/node-pre-gyp/archive/v0.6.5-appveyor.tar.gz
 call npm install https://github.com/mongodb-js/node-gyp/archive/v1.04-appveyor.tar.gz
-call npm install --build-from-source --msvs=2013
+call npm install --build-from-source
 call npm test
